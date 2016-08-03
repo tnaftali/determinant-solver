ba5f0185b1244fee7482f6b21b190fd3f72bbb0e
Tobias Naftali <tobias.naftali@gmail.com>
2016-08-03 07:59:03 -0300

    Code cleaning

--- Classes.py
+++ Classes.py
@@ -10,25 +10,6 @@ class Pivot(object):
             dec = decimal.Decimal(matrix[0][i]) * decimal.Decimal(inverse_of_first)
             matrix[0][i] = format(dec, '.10f')
         self.new_matrix = matrix
-    @staticmethod
-    def search_for_1_in_matrix(matrix):
-        result = []
-        exists = False
-        length = len(matrix)
-        i = 0
-        while i < length and not exists:
-            j = 0
-            while j < length and not exists:
-                if matrix[i][j] == 1:
-                    result.append(i)
-                    result.append(j)
-                    exists = True
-                j += 1
-            i += 1
-        if exists:
-            return result
-        else:
-            return None
 
     @staticmethod
     def get_inverse(num):
